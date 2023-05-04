e7d8301a101c927ff41c8160cfec049cbadd975d
Tara Skoglund <skoglundtara@gmail.com>
2023-05-04 16:01:07 +0200

    username vissas i consol

--- client/src/Pages/WelcomePage.tsx
+++ client/src/Pages/WelcomePage.tsx
@@ -1,7 +1,23 @@
 import { Box, Button, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { io, Socket } from "socket.io-client";
+
+const socket: Socket = io();
 
 export default function WelcomePage() {
+  const [username, setUsername] = useState("");
+
+  console.log(username);
+
+  const handleLogin = () => {
+    socket.emit("login", username);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
   return (
     <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
       <Box
@@ -16,11 +32,24 @@ export default function WelcomePage() {
         <Text fontSize={20}>vänligen skriv ditt namn:</Text>
       </Box>
       <Box m={"2rem"}>
-        <Input placeholder="large size" size="lg" w={"11.5rem"} />
+        <Input
+          placeholder="large size"
+          size="lg"
+          w={"11.5rem"}
+          type="text"
+          id="username"
+          name="username"
+          onChange={handleInputChange}
+        />
       </Box>
       <Box>
-        <Link to={"/startpage"}>
-          <Button variant="solid" borderRadius="25px">
+        <Link to="/startpage">
+          <Button
+            variant="solid"
+            borderRadius="25px"
+            id="submit"
+            onClick={handleLogin}
+          >
             Börja chatta!
           </Button>
         </Link>
