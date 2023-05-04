01c1d9952ee3c34b52a60c8429279dacc555f8c6
MadeleineGustafson <madeleinegu@gmail.com>
2023-05-04 14:55:40 +0200

    write chat message

--- client/src/components/ChattBox.tsx
+++ client/src/components/ChattBox.tsx
@@ -1,36 +1,71 @@
 import { Box, FormControl, Icon, Input, Stack } from "@chakra-ui/react";
+import { SetStateAction, useEffect, useState } from "react";
 import { IoReturnDownBackOutline, IoSend } from "react-icons/io5";
+import { useSocket } from "../../src/context/SocketContext";
 import SpeechBubble from "./SpeechBubble";
 
 function ChattBox() {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const { socket } = useSocket();
+
+  const [inputValue, setInputValue] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+  }, [socket]);
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (inputValue.trim() !== "") {
+      setMessages([...messages, inputValue]); // Add input value to messages
+      socket.emit("message", inputValue);
+      setInputValue("");
+      setSubmitted(true);
+    }
+  };
+
+  const handleInputChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => setInputValue(e.target.value);
+
   return (
     <>
       <Box sx={chatBox}>
         <Icon as={IoReturnDownBackOutline} boxSize={6} />
 
         <Box sx={chatBody}>
-          <SpeechBubble />
+          <SpeechBubble messages={messages} submitted={submitted} />
         </Box>
-        <form>
+
+        <form onSubmit={handleSubmit}>
           <Stack spacing={4} sx={flex}>
             <FormControl id="input">
-              {/* <FormLabel>Enter your input</FormLabel> */}
               <Input
                 sx={input}
                 type="text"
-                // value={inputValue}
-                // onChange={handleInputChange}
+                value={inputValue}
+                onChange={handleInputChange}
                 placeholder="Skriv ett meddelande..."
               />
             </FormControl>
-            <Icon as={IoSend} boxSize={6} type="submit" />
-            {/* <Button type="submit"></Button> */}
+            <Icon
+              as={IoSend}
+              boxSize={6}
+              type="submit"
+              onClick={handleSubmit}
+            />
           </Stack>
         </form>
       </Box>
     </>
   );
 }
+
+export default ChattBox;
 
 const input = {
   width: "25rem",
@@ -56,8 +91,6 @@ const chatBox = {
   justifyContent: "center",
   alignItems: "center",
   width: "40%",
-  height: "42rem",
+  minHeight: "42rem",
   borderRadius: "1.3rem",
 };
-
-export default ChattBox;
