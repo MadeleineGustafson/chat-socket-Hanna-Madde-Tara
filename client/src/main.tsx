1440b6fe172fc5a76e93b3411549f072f38dada6
Tara Skoglund <skoglundtara@gmail.com>
2023-05-04 10:51:27 +0200

    add pages and routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { io } from "socket.io-client";
 import App from "./App";
 import "./index.css";
 
 export const socket = io("ws://localhost:3000");
 
+import StartPage from "./Pages/StartPage";
+import WelcomePage from "./Pages/WelcomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/startpage" element={<StartPage />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
