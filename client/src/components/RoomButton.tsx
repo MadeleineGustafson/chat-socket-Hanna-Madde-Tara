5b8c14bb1d9fa7894bb8e5ea67b6538ada4ab24e
HaNNa89 <petersson.h@hotmail.com>
2023-05-05 13:21:31 +0200

    updated RoomButton

--- client/src/components/RoomButton.tsx
+++ client/src/components/RoomButton.tsx
@@ -1,12 +1,11 @@
-import { Box, Flex, Stack, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 function RoomButton() {
     return (
         <>
             <Flex justifyContent="center" bg="pink.400" width="200px">
                 <Box>
                     <Text as="h1" mb="4" fontSize="25" fontWeight="700">Aktiva rum</Text>
-                        <Stack mb="4">
-                        <Box as='button'
+                       <Box as='button'
                         height='34px'
                         width="150px"
                         border='none'
@@ -16,40 +15,6 @@ function RoomButton() {
                         fontWeight='semibold'
                         bg="pink.100"
                         >Rum 1</Box>
-
-                        <Box as='button'
-                        height='34px'
-                        width="150px"
-                        border='none'
-                        px='8px'
-                        borderRadius='8px'
-                        fontSize='14px'
-                        fontWeight='semibold'
-                        bg="pink.100"
-                        >Rum 2</Box>
-
-                        <Box as='button'
-                        height='34px'
-                        width="150px"
-                        border='none'
-                        px='8px'
-                        borderRadius='8px'
-                        fontSize='14px'
-                        fontWeight='semibold'
-                        bg="pink.100"
-                        >Rum 3</Box>
-
-                        <Box as='button'
-                        height='34px'
-                        width="150px"
-                        border='none'
-                        px='8px'
-                        borderRadius='8px'
-                        fontSize='14px'
-                        fontWeight='semibold'
-                        bg="pink.100"
-                        >Rum 4</Box>
-                       </Stack>
                 </Box>
             </Flex>
         </>
