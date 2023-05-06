341859dd7df773069cc14a8efbbaf8ebb52d4220
HaNNa89 <petersson.h@hotmail.com>
2023-05-06 18:58:44 +0200

    create a button, input + test layout

--- client/src/components/CreateNewRoomBtn.tsx
+++ client/src/components/CreateNewRoomBtn.tsx
@@ -1,8 +1,36 @@
-import { Text } from '@chakra-ui/react';
+import { Box, IconButton, Input, Text } from '@chakra-ui/react';
+import { TbSquareRoundedPlus } from "react-icons/tb";
 function CreateNewRoomBtn() {
     return (
         <>
-            <Text>Hej CreateNewRoomBtn!</Text>
+            <Box bg="pink">
+                <Text as='h1' mb="4">Skapa ett nytt rum</Text>
+                <form>
+                    <Box display="flex" flexDirection="row" gap="2" flex="1">
+                        <Input
+                        size="md"
+                        w="10em"
+                        type="text"
+                        //value={name}
+                        name="roomname"
+                        />            
+                        <IconButton
+                        variant='outline'
+                        colorScheme='yellow'
+                        aria-label='plus'
+                        fontSize='40px'
+                        border="none"
+                        icon={<TbSquareRoundedPlus />}>
+                        </IconButton>
+                            {/*<Button
+                                size="md"
+                                border="1px" 
+                                borderColor="yellow"
+                                borderRadius="10"> + </Button>
+                            */}
+                    </Box>
+                </form>
+            </Box>
         </>
     );
 }
