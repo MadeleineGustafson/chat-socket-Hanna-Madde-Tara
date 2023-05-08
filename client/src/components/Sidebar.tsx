9dc29b8c80f5e3fd276bc8de2b8b7a179b037f04
HaNNa89 <petersson.h@hotmail.com>
2023-05-08 15:10:08 +0200

    updated sidebar, deleted joinRoomForm

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { Box, Flex } from '@chakra-ui/react';
 import CreateNewRoomBtn from './CreateNewRoomBtn';
-import JoinRoomForm from './JoinRoomForm';
 import RoomButton from './RoomButton';
 
 function Sidebar() {
@@ -10,7 +9,7 @@ function Sidebar() {
           <Flex height="100vh">
             <Box bg="pink" width="20em" >
             <CreateNewRoomBtn />            
-            <JoinRoomForm />            
+                        
               <RoomButton />           
             </Box>
           </Flex>
