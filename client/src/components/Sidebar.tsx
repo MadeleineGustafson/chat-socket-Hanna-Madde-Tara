cadf93f05f398098fb4c1210985057af38583d7c
MadeleineGustafson <madeleinegu@gmail.com>
2023-05-08 15:17:58 +0200

    sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,15 +1,22 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex } from "@chakra-ui/react";
+import CreateNewRoomBtn from "./CreateNewRoomBtn";
+import JoinRoomForm from "./JoinRoomForm";
+import RoomButton from "./RoomButton";
 
 function Sidebar() {
-  
-    return (
-        <>
-          <Flex height="100vh">
-            <Box bg="pink" width="20em" />
-            <Box flex="1" bg="white" />
-          </Flex>
-        </>
-    );
+  return (
+    <>
+      <Flex height="100vh">
+        <Box bg="pink" width="20em">
+          <CreateNewRoomBtn></CreateNewRoomBtn>
+          <JoinRoomForm></JoinRoomForm>
+          <RoomButton></RoomButton>
+        </Box>
+        {/* <Box bg="pink" width="20em" />
+            <Box flex="1" bg="white" /> */}
+      </Flex>
+    </>
+  );
 }
 
 export default Sidebar;
